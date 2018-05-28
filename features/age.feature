8a9bd84a3b1872d6f2a6c3f37738392f1fca952e
Edgar <zorro.ej@gmail.com>
2018-05-29 07:20:23 +0800

    fix bug with cli

--- features/age.feature
+++ features/age.feature
@@ -1,18 +1,26 @@
 Feature: Age
-  A CLI to calculate age
+	A CLI to calculate age
 
 	Scenario: Use --dob
 		When I run `my_age is --dob 1981-03-08 --as_of 1989-03-07`
 		Then the output should contain "7"
 
-  Scenario: Use active_support date_time helper
+	Scenario: Use active_support date_time helper
 		When I run `my_age is -d today -a next_year`
 		Then the output should contain "1"
+
+	Scenario: Use active_support date_time years_since
+		When I run `my_age is -d today -a 'years_since(4)'`
+		Then the output should contain "4"
+
+  Scenario: Use active_support prev_month
+		When I run `my_age is -d 'years_ago(3)' -a prev_month`
+		Then the output should contain "2"
 
 	Scenario: Use -d and -a
 		When I run `my_age is -d 1981-01-02 -a 1991-01-02`
 		Then the output should contain "10"
-		
+
 	Scenario: default to today 
 		When I run `my_age is -d last_year`
 		Then the output should contain "1"
--- lib/my_age/cli.rb
+++ lib/my_age/cli.rb
@@ -1,6 +1,6 @@
 require 'thor'
 require 'my_age'
-
+require 'pry'
 module MyAge
 	class CLI < Thor
     attr_accessor :dob
@@ -10,9 +10,28 @@ module MyAge
 		method_option :dob, aliases: "d", desc: "date of birth in yyyy-mm-dd", required: true
 		method_option :as_of, aliases: "a", desc: "as of date in yyyy-mm-dd", required: false 
 		def is
-			self.dob = DateTime.strptime(options[:dob], "%Y-%m-%d")
-			date = options[:as_of].present? ? DateTime.strptime(options[:as_of], "%Y-%m-%d") : Date.today
+			self.dob = get_date(options[:dob]) 
+			date = options[:as_of].present? ? get_date(options[:as_of]) : Date.today
 			puts self.age(date)
+		end
+
+
+		private
+		def get_date(date)
+			begin
+      if date =~ /^\d{4}-\d{2}-\d{2}$/
+				 DateTime.strptime(date, "%Y-%m-%d")
+			else
+				dates = date.gsub(")", "").split("(")
+				if dates.length == 2
+				  Date.today.send(dates[0].to_sym, dates[1].to_i)
+				else
+				  Date.today.send(dates[0].to_sym)
+				end
+			end
+		rescue NoMethodError
+			Date.send(date)
+		end
 		end
 
 	end
